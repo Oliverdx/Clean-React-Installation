f406692d468eca663eb077440727b5fada3dd4a1
Vanderlei Oliveira <vanderlei.oliveira@oowlish.com>
2024-01-31 18:56:46 -0300

    2024 update with typescript

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Clean React Installation
-This is a light alternative to create a react aplication. Babel, Sass and Webpack is already setup just install and start to code
+This is a light alternative to create a react aplication, already setup just install and start to code
 
 ## INSTALLATION
 
-Pull this branch to your PC and apply `npm install` in the command line
+Pull this branch to your PC and apply `yarn` in the command line
 
 ## AUTHOR
 Made By [Vanderlei Oliveira](https://oliverdx.com.br)
--- webpack.common.js
+++ webpack.common.js
@@ -1,53 +1,29 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 
 module.exports = {
-  devtool: 'source-map',
-  entry: "./src/index.js",
+  entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js"
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react']
-        }
-      }
-    }, {
-      test: /\.s[ac]ss$/i,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'sass-loader'
-        }
-      ]
-    },
-    {
-      test: /\.(png|jpg|gif|ttf)$/,
-      use: [
-        {
-          loader: 'file-loader'
-        }
-      ]
-    }
-    ]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       hash: true,
       filename: "index.html",  //target html
       template: "./src/index.html" //source html
-    }),
-    new ExtractTextPlugin({ filename: 'css/style.css' })
+    })
   ]
-}
+};
